b99ab8da15ac2aa0ff6a2597916e53b4b7059daf
Davi Ramon <eudavi.ramon@gmail.com>
2022-01-10 14:38:57 -0300

    adicionado Model

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const express = require('express')
 const exphbs = require('express-handlebars')
 const app = express()
 const conn = require('./db/conn')
+const Task = require('./models/Task')
 
 app.engine('handlebars', exphbs.engine())
 app.set('view engine', 'handlebars')
@@ -27,9 +28,9 @@ app.use(express.static('public')) // arquivos estáticos
 
 // aplicação não funciona sem as tabelas criadas.
 conn
-.sync()  
-//.sync({force: true}) /*recria as tabelas. CUIDADO APAGA TODOS OS DADOS*/ 
-.then(() => {
-    app.listen(3000)
-})
-.catch((err) => console.log(err)) 
+    .sync()  
+    //.sync({force: true}) /*recria as tabelas. CUIDADO APAGA TODOS OS DADOS*/ 
+    .then(() => {
+        app.listen(3000)
+    })
+    .catch((err) => console.log(err)) 
